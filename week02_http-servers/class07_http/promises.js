09c10bcea6737b780f0c8601e2b81486fe94c0e9
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-15 14:09:52 -0800

    rick and morty

--- week02_http-servers/class07_http/promises.js
+++ week02_http-servers/class07_http/promises.js
@@ -11,10 +11,11 @@ const fs = require('fs');
 const readPromise = src => new Promise((resolve, reject) => {
   // read our file the old callback way
   fs.readFile(src, { encoding: 'utf8' }, (err, data) => {
-
+    // invoke the reject function with an error if unsuccessful
+    if(err) return reject(err);
+    // invoke the resolve function with data if successful
+    resolve(data);
   });
-  // invoke the resolve function with data if successful
-  // invoke the reject function with an error if unsuccessful
 });
 
 readPromise('./http.md')
