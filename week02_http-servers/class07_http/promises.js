5c076e01bf2eeefeaa2d49e8ce658d4a9bc5505a
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-15 13:17:05 -0800

    promise check

--- week02_http-servers/class07_http/promises.js
+++ week02_http-servers/class07_http/promises.js
@@ -1,9 +1,21 @@
-const fsPromises = require('fs').promises
+const fs = require('fs');
 
 // pending -> waiting for the promise to finish
 // fulfilled -> the promise finished and resolved
 // rejected -> the promise finished and rejected
-fsPromises.readFile('./http.md', { encoding: 'utf8' })
-  .then(data => fsPromises.writeFile('./http-copy.md', data))
-  .then(() => console.log('DONE'))
-  .catch(err => console.error(err));
+// fsPromises.readFile('./http.md', { encoding: 'utf8' })
+//   .then(data => fsPromises.writeFile('./http-copy.md', data))
+//   .then(() => console.log('DONE'))
+//   .catch(err => console.error(err));
+
+const readPromise = src => new Promise((resolve, reject) => {
+  // read our file the old callback way
+  fs.readFile(src, { encoding: 'utf8' }, (err, data) => {
+
+  });
+  // invoke the resolve function with data if successful
+  // invoke the reject function with an error if unsuccessful
+});
+
+readPromise('./http.md')
+  .then(data => console.log(data));
