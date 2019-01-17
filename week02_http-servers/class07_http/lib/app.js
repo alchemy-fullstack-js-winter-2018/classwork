9dbf0dc8cd66cd1ac8948e6df4f9b7cb3bacce75
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-17 10:25:25 -0800

    start people

--- week02_http-servers/class07_http/lib/app.js
+++ week02_http-servers/class07_http/lib/app.js
@@ -1,24 +1,54 @@
+const bodyParser = require('./bodyParser');
 const { parse } = require('url');
-const {
-  getCharacter,
-  getCharacters
-} = require('./service/rickAndMortyApi');
+const { getCharacters, getCharacter } = require('./service/rickAndMortyApi');
+
+const notes = {};
+
+const pushNote = (id, note) => {
+  if(!notes[id]) {
+    notes[id] = [];
+  }
+
+  notes[id].push(note);
+};
 
 module.exports = (req, res) => {
   const url = parse(req.url, true);
-  if(url.pathname.includes('/characters')) {
-    // get id of character
-    // split on /
-    // [character, :id]
-    const id = url.pathname.slice(1).split('/')[1];
+  if(req.method === 'GET' && url.pathname === '/characters') {
     getCharacters()
       .then(characters => {
         res.setHeader('Content-Type', 'text/html');
-        res.end('<html><body></body></html>');
-      })
-      .catch(err => {
-        res.statusCode = 500;
-        res.end(`Error ${err}`);
+        res.write('<html><body><ul>');
+        characters.forEach(character => {
+          res.write(`<li>${character.name}</li>`);
+        });
+        res.end('</body></html>');
+      });
+  } else if(req.method === 'POST' && url.pathname === '/characters') {
+    bodyParser(req)
+      .then(({ characterId: id, note }) => {
+        pushNote(id, note);
+        res.statusCode = 204;
+        res.end();
+      });
+  } else if(req.method === 'GET' && url.pathname.includes('/characters/')) {
+    const id = url.pathname.slice(1).split('/')[1];
+    const notesForCharacter = notes[id];
+    getCharacter(id)
+      .then(character => {
+        const noteItems = notesForCharacter
+          .reduce((acc, note) => {
+            acc += `<li>${note}</li>`;
+            return acc;
+          }, '');
+
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body>
+          <h1>${character.name}</h1>
+          <ul>
+            ${noteItems}
+          <ul>
+        </body></html>`);
       });
   }
 };
--- week02_http-servers/class07_http/test/app.test.js
+++ week02_http-servers/class07_http/test/app.test.js
@@ -3,16 +3,34 @@ const app = require('../lib/app');
 
 jest.mock('../lib/service/rickAndMortyApi.js');
 
-describe('app', () => {
-  it('gets a character by id', () => {
+describe('rick and morty api tests', () => {
+  it('gets a list of characters', () => {
     return request(app)
-      .get('/character/1')
+      .get('/characters')
       .then(res => {
-        expect(res.body).toEqual({
-          name: 'Rick Sanchez',
-          species: 'Human',
-          status: 'Alive'
-        });
+        expect(res.text).toContain('Rick');
+      });
+  });
+
+  it('saves a note for a character', () => {
+    return request(app)
+      .post('/characters')
+      .send({ characterId: 1, note: 'Great character' })
+      .then(res => {
+        expect(res.status).toEqual(204);
+      });
+  });
+
+  it('gets notes for a character', () => {
+    return request(app)
+      .post('/characters')
+      .send({ characterId: 1, note: 'Great character' })
+      .then(() => {
+        return request(app)
+          .get('/characters/1');
+      })
+      .then(res => {
+        expect(res.text).toContain('Great character');
       });
   });
 });
