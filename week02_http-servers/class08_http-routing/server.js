e40472f4b83cf81e2dce38348729e6ebd07d8417
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-17 13:46:42 -0800

    get

--- week02_http-servers/class08_http-routing/server.js
+++ week02_http-servers/class08_http-routing/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const app = require('./lib/app');
 
+const PORT = 7890 || process.env.PORT;
+
 http
   .createServer(app)
-  .listen(7890, () => console.log('Listening on 7890'));
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
