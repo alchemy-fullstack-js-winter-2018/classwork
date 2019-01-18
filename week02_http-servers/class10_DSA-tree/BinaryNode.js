aa6105d798723b84b5c550261f9629e534d004c5
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-18 11:49:55 -0800

    rec

--- week02_http-servers/class10_DSA-tree/BinaryNode.js
+++ week02_http-servers/class10_DSA-tree/BinaryNode.js
@@ -43,3 +43,22 @@ B.add(C);
 B.add(E)
 
 console.log(B);
+
+
+console.log(fib(5))
+
+function factorial(n) {
+    if (n === 1) {
+        return 1
+    }
+
+    return n * factorial(n - 1);
+}
+
+function fib(n) {
+    if (n <= 0) {
+        return 1
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
