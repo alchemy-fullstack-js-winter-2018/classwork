03e8e3ab481902c4e5cbfa8df9d03e41d6f9977a
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-18 11:31:44 -0800

    add

--- week02_http-servers/class10_DSA-tree/BinaryNode.js
+++ week02_http-servers/class10_DSA-tree/BinaryNode.js
@@ -2,13 +2,33 @@
 
 class BinaryNode {
     constructor(data) {
-        this.data = value;
+        this.data = data;
         this.left = null;
         this.right = null;
     }
 
     add(node) {
-        // Implement Me!
+        if (node.data < this.data) {
+            // going left
+            if (this.left) {
+                // left already exists
+                this.left.add(node);
+            } else {
+                // left is empty
+                this.left = node;
+            }
+        }
+
+        if (node.data >= this.data) {
+            // going right
+            if (this.right) {
+                // right already exists
+                this.right.add(node);
+            } else {
+                // right is empty
+                this.right = node;
+            }
+        }
     }
 }
 
@@ -16,8 +36,10 @@ const A = new BinaryNode('A');
 const B = new BinaryNode('B');
 const C = new BinaryNode('C');
 const D = new BinaryNode('D');
+const E = new BinaryNode('E');
 B.add(A);
 B.add(D);
 B.add(C);
+B.add(E)
 
-
+console.log(B);
