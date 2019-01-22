63f2b39b6e68662b1ede3c2f9cb1e7d989b82ff2
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-22 09:15:41 -0800

    tweets

--- week02_http-servers/class09_express/test/app.test.js
+++ week02_http-servers/class09_express/test/app.test.js
@@ -4,14 +4,22 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('tweets', () => {
+
+  const createTweet = (text, handle = 'ryan') => {
+    return request(app)
+      .post('/tweets')
+      .send({ handle, text })
+      .then(res => res.body);
+  };
+
   beforeEach(done => {
-    rimraf('./data/people', err => {
+    rimraf('./data/tweets', err => {
       done(err);
     });
   });
 
   beforeEach(done => {
-    mkdirp('./data/people', err => {
+    mkdirp('./data/tweets', err => {
       done(err);
     });
   });
@@ -30,6 +38,55 @@ describe('tweets', () => {
   });
 
   it('gets a list of tweets', () => {
+    return Promise.all(['hi', 'a tweet'].map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+      });
+  });
 
+  it('gets a tweet by id', () => {
+    return createTweet('my first tweet')
+      .then(tweet => {
+        return Promise.all([
+          Promise.resolve(tweet._id),
+          request(app)
+            .get(`/tweets/${tweet._id}`)
+        ]);
+      })
+      .then(([_id, res]) => {
+        expect(res.body).toEqual({
+          text: 'my first tweet',
+          handle: 'ryan',
+          _id
+        });
+      });
+  });
+
+  it('can update a tweet', () => {
+    return createTweet('a twet')
+      .then(tweet => {
+        const id = tweet._id;
+        return request(app)
+          .put(`/tweets/${id}`)
+          .send({ ...tweet, text: 'a tweet' });
+      })
+      .then(res => {
+        expect(res.body.text).toEqual('a tweet');
+      });
+  });
+
+  it('can delete a tweet', () => {
+    return createTweet('a tweet')
+      .then(tweet => {
+        return request(app)
+          .delete(`/tweets/${tweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ deleted: 1 });
+      });
   });
 });
