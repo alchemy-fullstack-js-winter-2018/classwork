98efcd3d935e6f5023dc7bf901fb282bf0997518
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-22 17:18:43 -0800

    getters

--- week03_mongodb/class11_mongodb/lib/routes/tweets.js
+++ week03_mongodb/class11_mongodb/lib/routes/tweets.js
@@ -8,4 +8,18 @@ module.exports = Router()
       .create({ handle, text })
       .then(tweet => res.send(tweet))
       .catch(next);
+  })
+  .get('/', (req, res, next) => {
+    Tweet
+      .find()
+      .then(tweets => res.send(tweets))
+      .catch(next);
+  })
+
+  .get('/:id', (req, res, next) => {
+    const _id = req.params.id;
+    Tweet
+      .findById(_id)
+      .then(foundTweet => res.send(foundTweet))
+      .catch(next);
   });
--- week03_mongodb/class11_mongodb/test/app.test.js
+++ week03_mongodb/class11_mongodb/test/app.test.js
@@ -3,8 +3,13 @@ require('../lib/utils/connect')();
 const app = require('../lib/app');
 const mongoose = require('mongoose');
 const request = require('supertest');
+const Tweet = require('../lib/models/Tweet');
 
 describe('tweets app', () => {
+  const createTweet = (handle, text = 'a tweet') => {
+    return Tweet.create({ handle, text })
+  }
+
   beforeEach(done => {
     return mongoose.connection.dropDatabase(() => {
       done();
@@ -22,6 +27,33 @@ describe('tweets app', () => {
         expect(res.body).toEqual({
           handle: 'ryan',
           text: 'hello tweets',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('finds a list of tweets', () => {
+    return Promise.all(['ryan', 'another handle'].map(createTweet))
+      .then(createdTweets => {
+        return request(app)
+          .get('/tweets')
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+      });
+  });
+
+  it('gets a tweet by id', () => {
+    return createTweet('ryan')
+      .then(createdTweet => {
+        return request(app)
+          .get(`/tweets/${createdTweet._id}`)
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ryan',
+          text: 'a tweet',
           _id: expect.any(String),
           __v: 0
         });
