8bf4ff4cb2eb44a806f8070097b0f71f1f466993
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-22 17:07:30 -0800

    updates

--- week03_mongodb/class11_mongodb/test/app.test.js
+++ week03_mongodb/class11_mongodb/test/app.test.js
@@ -1,2 +1,30 @@
 require('dotenv').config();
-require('./lib/utils/connect')();
+require('../lib/utils/connect')();
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+describe('tweets app', () => {
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+
+  it('can create a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'ryan',
+        text: 'hello tweets'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ryan',
+          text: 'hello tweets',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+});
