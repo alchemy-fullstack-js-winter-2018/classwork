1db08f08595825f8bc9fb9b75d45a16b5840fef1
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-29 10:27:55 -0800

    signin

--- week03_mongodb/class13_auth/lib/routes/auth.js
+++ week03_mongodb/class13_auth/lib/routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const User = require('../models/User');
+const { HttpError } = require('../middleware/error');
 
 module.exports = Router()
   .post('/signup', (req, res, next) => {
@@ -7,5 +8,33 @@ module.exports = Router()
     User
       .create({ email, password })
       .then(user => res.send({ user, token: user.authToken() }))
+      .catch(next);
+  })
+
+  .post('/signin', (req, res, next) => {
+    const { email, password } = req.body;
+
+    User
+      .findOne({ email })
+      .then(user => {
+        if(!user) {
+          return next(new HttpError(401, 'Bad email or password'));
+        }
+        
+        return Promise.all([
+          Promise.resolve(user),
+          user.compare(password)
+        ]);
+      })
+      .then(([user, correct]) => {
+        if(correct) {
+          res.send({
+            user,
+            token: user.authToken()
+          });
+        } else {
+          next(new HttpError(401, 'Bad email or password'));
+        }
+      })
       .catch(next);
   });
