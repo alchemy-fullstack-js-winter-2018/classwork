dda42fd32a83a9f3cab5f8e53e4abb4fc537c973
Ryan Mehta <ryan.mehta@gmail.com>
2019-01-31 10:18:11 -0800

    fix seed

--- week04_midproject-prep/class16_aggregation/seed.js
+++ week04_midproject-prep/class16_aggregation/seed.js
@@ -3,6 +3,6 @@ require('./lib/utils/connect')();
 const mongoose = require('mongoose');
 const seedData = require('./test/seedData');
 
-seedData()
+seedData({})
   .then(() => console.log('done'))
   .finally(() => mongoose.connection.close());
