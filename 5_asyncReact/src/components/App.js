0c9b1488d9ac3a540be96f4431050f2c3f2d5d04
Ryan Mehta <ryan.mehta@gmail.com>
2019-02-20 21:53:17 -0800

    async

--- 5_asyncReact/src/components/App.js
+++ 5_asyncReact/src/components/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Characters from './characters/Characters';
 
 export default class App extends Component {
+  state = {
+    currentPage: 1,
+    totalPages: 3
+  };
+
   render() {
     return (
       <Characters />
