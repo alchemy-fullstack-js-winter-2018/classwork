0adf69309073df92b484649594134ca9200b4ecf
Ryan Mehta <ryan.mehta@gmail.com>
2019-03-04 15:26:38 -0800

    done

--- 10_reduxPerf/src/reducers/words.js
+++ 10_reduxPerf/src/reducers/words.js
@@ -1,9 +1,16 @@
 import dictionary from '../fixtures/words.json';
+import { UPDATE_SEARCH_TERM } from '../actions/words.js';
 
 const initialState = {
-  dictionary
+  dictionary,
+  searchTerm: ''
 };
 
 export default function reducer(state = initialState, action) {
-  return state;
+  switch(action.type) {
+    case UPDATE_SEARCH_TERM:
+      return { ...state, searchTerm: action.payload };
+    default:
+      return state;
+  }
 }
