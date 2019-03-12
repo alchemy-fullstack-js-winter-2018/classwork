460fbaf7fc9899c696be723381f207598bd4e022
Ryan Mehta <ryan.mehta@gmail.com>
2019-03-12 15:47:41 -0700

    add cloudinary

--- 12_devsign/src/components/header/Header.js
+++ 12_devsign/src/components/header/Header.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Header.css';
 
 export default function Header() {
   return (
     <header className={styles.Header}>
-      <h1>My News Group</h1>
-
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
+      <h1>Looner</h1>
     </header>
   );
 }
